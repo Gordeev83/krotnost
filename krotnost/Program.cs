7b8a9723e5ef8e803b6e5e87521f71d0dd95f1e5
Gordeev83 <152768178+Gordeev83@users.noreply.github.com>
2024-02-21 08:34:25 +0300

    Добавьте файлы проекта.

---
+++ krotnost/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace krotnost
+{
+     class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите число от 1 до 100:");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number < 1 || number > 100)
+            {
+                Console.WriteLine("Ошибка! Число должно быть от 1 до 100");
+            }
+            else if (number % 3 == 0 && number % 5 == 0)
+            {
+                Console.WriteLine("Fizz Buzz");
+            }
+            else if (number % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (number % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+
+}
